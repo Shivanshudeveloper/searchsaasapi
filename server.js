dd2779b5478cb46d4b32b3d68eb045002e23acc4
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2021-10-07 14:29:37 +0530

    New

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
-//const BlogPost=require('./models/BlogPost')
 const express = require("express");
-const morgan = require("morgan");
-const routes = require("./routes/api");
-
 const path = require("path");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ message: "HI" });
-});
-
+const mongoose = require("mongoose");
+const colors = require("colors");
 const cors = require("cors");
-
+// Route Files
+const routes = require("./routes/api");
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
-
+app.use(express.json());
+// Routing for API Service
+app.use("/api", routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(morgan("tiny"));
-app.use("/api", routes);
-
-app.listen(PORT, console.log(`Server is running at ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
