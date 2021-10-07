acc5d8b92d233a33f3124749b18691e9eb312f87
Manav Nanwani <manav.nanwani@gmail.com>
2021-10-07 14:54:34 +0530

    Check

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const colors = require("colors");
 const cors = require("cors");
 const morgan = require("morgan");
-// Route Files
+const dotenv = require("dotenv");
+
 const routes = require("./routes/api");
+
 const app = express();
+dotenv.config();
+
 app.use(cors());
-app.use(express.json());
-// Routing for API Service
+
 app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome !!");
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
